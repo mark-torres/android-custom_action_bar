39afc6aebab9bc091519dc21b01afd1fb843ea43
Mark Torres <mark.torres.mets@gmail.com>
2018-02-22 16:22:35 -0600

    Add actions to buttons

--- app/src/main/java/com/hsoftmobile/customactionbar/MainActivity.java
+++ app/src/main/java/com/hsoftmobile/customactionbar/MainActivity.java
@@ -6,7 +6,9 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 // SOURCE: http://stacktips.com/tutorials/android/actionbar-with-custom-view-example-in-android
 
@@ -28,12 +30,35 @@ public class MainActivity extends AppCompatActivity {
 
 			LayoutInflater inflater = LayoutInflater.from(this);
 			View customView = inflater.inflate(R.layout.custom_action_bar, null);
-			TextView labelTitle = (TextView) customView.findViewById(R.id.label_title);
-			labelTitle.setText("My action bar");
-
 			ActionBar.LayoutParams layoutParams = new ActionBar.LayoutParams(ActionBar.LayoutParams.FILL_PARENT, ActionBar.LayoutParams.FILL_PARENT);
 			actionBar.setCustomView(customView, layoutParams);
 			actionBar.setDisplayShowCustomEnabled(true);
+
+			// bind views
+			TextView labelTitle = (TextView) customView.findViewById(R.id.label_title);
+			ImageButton btnBack = (ImageButton) customView.findViewById(R.id.btn_back);
+			ImageButton btnFwd = (ImageButton) customView.findViewById(R.id.btn_fwd);
+
+			// set title
+			labelTitle.setText("My action bar");
+
+			// set buttons click listeners
+			btnBack.setOnClickListener(new View.OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					showToast("Clicked back button");
+				}
+			});
+			btnFwd.setOnClickListener(new View.OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					showToast("Clicked forward button");
+				}
+			});
 		}
+	}
+
+	private void showToast(String message) {
+		Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
 	}
 }
