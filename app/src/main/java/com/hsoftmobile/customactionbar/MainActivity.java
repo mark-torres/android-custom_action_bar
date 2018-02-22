f233ea57cc41682b9d560f720d5a8ea6ee65d03d
Mark Torres <mark.torres.mets@gmail.com>
2018-02-22 17:40:50 -0600

    Add menu to action bar

--- app/src/main/java/com/hsoftmobile/customactionbar/MainActivity.java
+++ app/src/main/java/com/hsoftmobile/customactionbar/MainActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
@@ -40,7 +42,6 @@ public class MainActivity extends AppCompatActivity {
 			// bind views
 			TextView labelTitle = customView.findViewById(R.id.label_title);
 			ImageButton btnBack = customView.findViewById(R.id.btn_back);
-			ImageButton btnFwd = customView.findViewById(R.id.btn_fwd);
 
 			// set title
 			labelTitle.setText("My action bar");
@@ -52,14 +53,30 @@ public class MainActivity extends AppCompatActivity {
 					showToast("Clicked back button");
 				}
 			});
-			btnFwd.setOnClickListener(new View.OnClickListener() {
-				@Override
-				public void onClick(View v) {
-					showToast("Clicked forward button");
-				}
-			});
 		}
 	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.main_menu, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+			case R.id.mitem_home:
+				showToast("Clicked home menu item");
+				return true;
+			case R.id.mitem_exit:
+				showToast("Clicked exit menu item");
+				return true;
+			default:
+				return super.onOptionsItemSelected(item);
+		}
+	}
+
+	// UTIL METHODS = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
 
 	private void showToast(String message) {
 		Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
